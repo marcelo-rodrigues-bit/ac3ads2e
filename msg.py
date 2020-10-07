18214888a7101095ae828f734769030423874af7
Marcelo Rodrigues <marcelo.rodrigues@aluno.faculdadeimpacta.com.br>
2020-10-06 22:36:17 -0300

    ac3

--- msg.py
+++ msg.py
@@ -7,10 +7,29 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
-    primos = "Tudo vai dar certo caros alunos! "
+    limite = 100
 
+    c = 1
+    p = 1
+    numero = 3
+
+    primos = "2,"
+
+    while p < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1
+            if(p % 10 == 0):
+                primos = primos + "<br>"
+        numero+=1
 
     return primos
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
