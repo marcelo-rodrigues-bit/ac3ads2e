6984f2b5ee37d41fe3dfdd60b115d3c8f6be62a7
Marcelo Rodrigues <marcelo.rodrigues@aluno.faculdadeimpacta.com.br>
2020-10-06 22:49:47 -0300

    ac3

--- msg.py
+++ msg.py
@@ -1,3 +1,4 @@
+  
 import os
 from flask import Flask, jsonify, request
 from math import sqrt
@@ -7,29 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
-    limite = 100
+    primos = "Tudo vai dar certo caros alunos! "
 
-    c = 1
-    p = 1
-    numero = 3
-
-    primos = "2,"
-
-    while p < limite:
-        ehprimo = 1
-        for i in range(2, numero):
-            if numero % i == 0:
-                ehprimo = 0
-                break
-        if (ehprimo):
-            primos = primos + str(numero) + ","
-            p += 1
-            if(p % 10 == 0):
-                primos = primos + "<br>"
-        numero+=1
 
     return primos
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
